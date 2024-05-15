aa2e261d27234dc55cbaeb0b3ee76206219aebf9
schecher <leonhard.schecher@gmail.com>
2024-05-15 18:12:02 +0300

    Esim

---
+++ GUIOOP/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module schecher.failid.guioop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens tripstrapstrull.failid.guioop to javafx.fxml;
+    exports tripstrapstrull.failid.guioop;
+}
